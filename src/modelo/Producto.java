2d8ffac6f2598ee86af4a25d180e90ebac6e82f8
Karen Garcia <karyhernandez1509@gmail.com>
2026-03-24 18:06:06 -0500

    actualizacion

---
+++ src/modelo/Producto.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author herna
+ */
+public class Producto {
+    private String codigo;
+    private String nombre;
+    private String proveedor;
+    private String impuesto;
+    private double costo;
+    private int stock;
+    private int stockMinimo;
+    private double precio;
+    private String cocina;
+    private String barra;
+    private String otros;
+    
+}
